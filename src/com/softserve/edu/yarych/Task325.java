156b37359b58284919eed39138a6cbd27f217951
andyyarych <flakerino@gmail.com>
2017-04-23 19:56:55 +0300

    modified Task224; added Task325

--- src/com/softserve/edu/yarych/Task325.java
+++ src/com/softserve/edu/yarych/Task325.java
@@ -2,17 +2,27 @@ package com.softserve.edu.yarych;
 
 import java.util.Scanner;
 
-public class Task224 {
+public class Task325 {
+
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		System.out.println("Enter natural number: ");
 		int number = sc.nextInt();
+		boolean isSimple = true;
+		
 		for (int i = 1; i <= number; i++) {
 			if ((number%i) == 0) {
-				System.out.println(i);
+				for (int j = 2; j <= Math.sqrt(i); j++) {
+					if (i%j == 0) {
+						isSimple = false;
+					}
+				}
+				if (isSimple) {
+					System.out.println(i);
+				}
 			}
 		}
+		
 	}
-
 }
