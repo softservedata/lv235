137cd5d872dbe07b0452f3727d425a0982613d83
V_Ghost <Zubchynskyi@gmail.com>
2017-04-23 15:08:35 +0300

    Added 3rd Task and fixed few bugs

--- src/NoPackage/App.java
+++ src/NoPackage/App.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 // Volodymyr Zubchyns'kyi
 //  86 а) 332 178 б)
+
 public class App {
 
-	 static Scanner input = new Scanner(System.in);
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	//	Task1();
-		Task2();
-
+		
+		TaskChooser();
 	}
 	
 	
@@ -43,10 +43,6 @@ public class App {
 		// тут можна написати switch для того, щоб правильно писалося слово "цифра" (напр 1 цифра, 2 цифри і т.д.)
 		System.out.println("число n "+ n + " має " + counter + " цифр");
 		}
-	
-	public static void checker() {
-		
-	}
 	
 	public static void Task2() {
 		System.out.println("Дано натуральне число n. Вказати такі невід'ємні x,y,z,t, що n=x^2+y^2+z^2+t^2");
@@ -86,7 +82,70 @@ public class App {
 		System.out.println();
 		
 		}
+	
+	public static void Task3() {
+		System.out.println("Дано натуральні числа n ,a_1..a_n. Оприділити кількість членів кратних 3 та некратних 5 ");
+		
+		int n = 0;
+		int a = 0;
+		boolean noArgs = true;
+		
+		System.out.print("n = ");
+		n= input.nextInt();
+		System.out.print("a = ");
+		a= input.nextInt();
+		int mas[] = new int [n+1];
+		for(int i=0; i<n; i++)
+		{
+			mas[i] = a;
+			a+=1;
+			if(mas[i]%3 == 0 && mas[i]%5 != 0)
+			{
+				System.out.println("Число a = " + mas[i] + " Кратне 3 та некратне 5" );
+				noArgs = false;
+			}
+		}
+		if(noArgs == true)
+		{
+			System.out.println("У даній послідовності немає чисел кратних 3 та некратних 5");
+		}
+		
 	}
+
+	//not used in code
+	public static void TryAgain() {
+		String checker;
+		System.out.println("Try again? 1/n: ");
+		checker = input.next();
+		if (checker == "y") {
+			TaskChooser();
+		}
+	}
+	
+	public static void TaskChooser() {
+		
+int taskChooser;
+		
+		System.out.println("Виберіть номер задачі 1, 2, 3");
+		System.out.print("Вибранй номер: ");
+		taskChooser = input.nextInt();
+		switch (taskChooser) {
+		case 1:
+			Task1();
+			break;
+
+		case 2:
+			Task2();
+			break;
+			
+		default:
+			Task3();
+			break;
+			
+		}
+	}
+
+}
 	
 	
 
