c31b158ffac7b01ff863bc040e0b12dae0be3deb
Ihor <tistyk.27@gmail.com>
2017-04-25 12:28:59 +0400

    First commit in a new branch

---
+++ src/com/softserve/edu/com/softserve/edu/Main.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.com.softserve.edu;
+
+/**
+ * Created by ${Ihor} on ${24.04.2017}.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world on my own branch - Ihor!");
+    }
+}
