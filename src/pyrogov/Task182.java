c2dced92dbe747ca638c7b7c33b0f775178a4776
andriyVP <andriy.pyrogov88@gmail.com>
2017-05-03 00:24:52 +0300

    minor fixing

--- src/pyrogov/Task182.java
+++ src/pyrogov/Task182.java
@@ -9,6 +9,7 @@
 package pyrogov;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * class with descriptions.
@@ -26,18 +27,28 @@ public class Task182 {
 		/**
 		 * Java doc.
 		 */
-		printCalculatedValues(10, -50);
+
+		System.out.println("Enter natural number n which defines array lenght");
+
+		Scanner sc = new Scanner(System.in);
+		int arrayLength = Integer.parseInt(sc.next());
+		System.out.println("Enter number which defines left border array");
+		int leftArrayBorder = Integer.parseInt(sc.next());
+
+		printCalculatedValues(arrayLength, leftArrayBorder);
+
 	}
 
-	public static void printCalculatedValues(final int arrayLength, final int leftArrayBorder) {
+	// метод для рахування значень
+	public static void printCalculatedValues(int arrayLength, int leftArrayBorder) {
 
-		final int[] numbers = new int[arrayLength];
+		int[] numbers = new int[arrayLength];
 		int arraySum = 0;
 		int count = 0;
 
 		for (int i = 0; i < numbers.length; i++) {
 
-			numbers[i] = (int) ((Math.random() * 100) + leftArrayBorder);
+			numbers[i] = (int) ((i * 100) + leftArrayBorder);
 
 			if (numbers[i] % 5 == 0 && numbers[i] % 7 != 0) {
 				arraySum += numbers[i];
@@ -47,9 +58,9 @@ public class Task182 {
 
 		}
 
-		System.out.println("Масив згенерованих чисел: " + Arrays.toString(numbers));
-		System.out.println("Кількість членів послідовності: " + count);
-		System.out.println("Сума членів послідовності: " + arraySum);
+		System.out.println("Generated numbers array : " + Arrays.toString(numbers));
+		System.out.println("Кількість членів послідовності які діляться на 5 і не діляться на 7: " + count);
+		System.out.println("Сума членів послідовності які діляться на 5 і не діляться на 7: " + arraySum);
 
 	}
 
