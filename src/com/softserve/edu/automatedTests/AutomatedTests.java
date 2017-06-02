855dd1443818bcab874c373ef6afcea9235e19a7
Volodymyr Zubchyns'kyi <vzubctc@training.local>
2017-06-02 12:26:39 +0300

    added Delay for demo

--- src/com/softserve/edu/automatedTests/AutomatedTests.java
+++ src/com/softserve/edu/automatedTests/AutomatedTests.java
@@ -1,5 +1,6 @@
 package com.softserve.edu.automatedTests;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -9,8 +10,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.beust.jcommander.internal.Console;
+
 public class AutomatedTests {
 
+	private final int DELAY_FOR_DEMO = 0;
 	/**
 	 * Thread.sleep() Use only for DEMO! This test will CREATE a new subclass.
 	 */
@@ -21,58 +25,58 @@ public class AutomatedTests {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("http://java.training.local:8080/registrator");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		// login
 		driver.findElement(By.id("login")).click();
 		driver.findElement(By.id("login")).clear();
 		driver.findElement(By.id("login")).sendKeys("regist");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("password")).click();
 		driver.findElement(By.id("password")).clear();
 		driver.findElement(By.id("password")).sendKeys("111111");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		// Subclass
 		driver.findElement(By.xpath("//*[@id='navigationbar']/ul/li[3]/a")).click(); // Subclass of objects
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.xpath("//*[@id='body']/p/a")).click(); // Add new subclass
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("typeName")).click();
 		driver.findElement(By.name("typeName")).sendKeys("Wifi");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("clickmeshow")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("parameters[0].description")).click();
 		driver.findElement(By.name("parameters[0].description")).sendKeys("Frequency");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("parameters[0].unitName")).click();
 		driver.findElement(By.name("parameters[0].unitName")).sendKeys("GGz");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("parameters[0].parametersType")).click();
 		driver.findElement(By.cssSelector("*[value='discreteParameters']")).click();
 		driver.findElement(By.id("btnAdd")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("parameters[1].description")).click();
 		driver.findElement(By.name("parameters[1].description")).sendKeys("Channel");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("parameters[1].unitName")).click();
 		driver.findElement(By.name("parameters[1].unitName")).sendKeys("ch");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.name("parameters[1].parametersType")).click();
 		WebElement dropdown2 = driver.findElement(By.cssSelector("#input2 #myparam2"));
 		dropdown2.findElement(By.cssSelector("*[value='linearParameters']")).click();
 		driver.findElement(By.id("valid")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		Assert.assertNotNull(driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'Wifi')]")).getText());
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		// exit
 		driver.findElement(By.cssSelector(".btn.btn-primary.btn-sm.dropdown-toggle")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.xpath("//a[contains(@href,'/logout')]")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		Assert.assertTrue(driver.findElement(By.cssSelector("img")).getAttribute("src").contains("/ukraine_logo2.gif"));
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.quit();
 	}
 
@@ -87,34 +91,38 @@ public class AutomatedTests {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("http://java.training.local:8080/registrator");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		// login
 		driver.findElement(By.id("login")).click();
 		driver.findElement(By.id("login")).clear();
 		driver.findElement(By.id("login")).sendKeys("regist");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("password")).click();
 		driver.findElement(By.id("password")).clear();
 		driver.findElement(By.id("password")).sendKeys("111111");
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		//Delete
 		driver.findElement(By.xpath("//*[@id='navigationbar']/ul/li[3]/a")).click(); // Subclass of objects
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		Assert.assertNotNull(driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'Wifi')]")).getText());
 		driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'Wifi')]/../td[6]/div/a")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector(".bootbox.modal.fade.bootbox-confirm.in")).findElement(By.cssSelector(".btn.btn-primary")).click();
-		Thread.sleep(1000);
-		//Assert.assertNull(driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'Wifi')]")).getText());
+		Thread.sleep(DELAY_FOR_DEMO);
+		//Test if not null
+		List<WebElement> list = driver.findElements(By.xpath("//*[@id='datatable']//td[contains(.,'Wifi')]"));
+		for (WebElement li : list) {
+			System.out.println(li.toString());
+		}
 		// exit
 		driver.findElement(By.cssSelector(".btn.btn-primary.btn-sm.dropdown-toggle")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.xpath("//a[contains(@href,'/logout')]")).click();
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		Assert.assertTrue(driver.findElement(By.cssSelector("img")).getAttribute("src").contains("/ukraine_logo2.gif"));
-		Thread.sleep(1000);
+		Thread.sleep(DELAY_FOR_DEMO);
 		driver.quit();
 	}
 }
