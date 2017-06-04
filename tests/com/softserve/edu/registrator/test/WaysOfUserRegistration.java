0532d82dc837e6a6fb8e7e36093070c6dd3d1447
Tymofii <tymofii.kritsak@gmail.com>
2017-06-04 17:44:56 +0300

    Refactored localization tests

--- tests/com/softserve/edu/registrator/test/WaysOfUserRegistration.java
+++ tests/com/softserve/edu/registrator/test/WaysOfUserRegistration.java
@@ -18,19 +18,25 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class Task1 {
+public class WaysOfUserRegistration {
 	private static ChromeDriverService service;
 	private static WebDriver driver;
-	private static final Logger LOG = Logger.getLogger(Task1.class);
+	private static final Logger LOG = Logger
+			.getLogger(WaysOfUserRegistration.class);
+	private static final String ADMIN_LOGIN = "admin";
+	private static final String ADMIN_PASSWORD = "admin";
+	private static final String COMMISSIONER_LOGIN = "NazarComis";
+	private static final String COMMISSIONER_PASSWORD = "qwerty";
 
 	@BeforeClass
 	public static void createService() throws IOException {
-		service = new ChromeDriverService.Builder().usingDriverExecutable(new File("resources/chromedriver.exe"))
+		service = new ChromeDriverService.Builder()
+				.usingDriverExecutable(new File("resources/chromedriver.exe"))
 				.usingAnyFreePort().build();
-		// .usingPort(8888).build();
 		service.start();
 		LOG.debug("+++Service Start");
-		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"resources/chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
 		options.addArguments("--disable-web-security");
@@ -47,64 +53,86 @@ public class Task1 {
 	}
 
 	private void logOff() {
-		driver.findElement(By.xpath("//*[@id='header']/div[2]/div[1]/div/button[2]")).click();
-		driver.findElement(By.xpath("//*[@id='header']/div[2]/div[1]/div/ul/li[4]/a")).click();
+		driver.findElement(
+				By.xpath("//*[@id='header']/div[2]/div[1]/div/button[2]"))
+				.click();
+		driver.findElement(
+				By.xpath("//*[@id='header']/div[2]/div[1]/div/ul/li[4]/a"))
+				.click();
 	}
 
 	private void logAsAdmin() {
-		driver.findElement(By.name("login")).sendKeys("tomtom");
-		driver.findElement(By.name("password")).sendKeys("qwerty");
-		driver.findElement(By.xpath("//*[@id='loginForm']/div[5]/button")).click();
+		driver.findElement(By.name("login")).sendKeys(ADMIN_LOGIN);
+		driver.findElement(By.name("password")).sendKeys(ADMIN_PASSWORD);
+		driver.findElement(By.xpath("//*[@id='loginForm']/div[5]/button"))
+				.click();
 	}
 
 	private void logAsCommissioner() {
-		driver.findElement(By.name("login")).sendKeys("tomutomu");
-		driver.findElement(By.name("password")).sendKeys("qwerty");
-		driver.findElement(By.xpath("//*[@id='loginForm']/div[5]/button")).click();
+		driver.findElement(By.name("login")).sendKeys(COMMISSIONER_LOGIN);
+		driver.findElement(By.name("password")).sendKeys(COMMISSIONER_PASSWORD);
+		driver.findElement(By.xpath("//*[@id='loginForm']/div[5]/button"))
+				.click();
 	}
 
 	@Test
 	public void bothRegistrationMethodsAreAvailableTest() {
 		logAsAdmin();
-		driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(3) > a")).click();
+		driver.findElement(
+				By.cssSelector("#navigationbar > ul > li:nth-child(3) > a"))
+				.click();
 		driver.findElement(
 				By.cssSelector("#ñhangeReg > div:nth-child(1) > div.panel-body.panel30 > div:nth-child(4) > label"))
 				.click();
 		driver.findElement(By.id("confirmRegistrationMethod")).click();
 		logOff();
-		Assert.assertTrue(driver.findElements(By.cssSelector("#loginForm > div:nth-child(5) > a")).size() > 0);
+		Assert.assertTrue(driver.findElements(
+				By.cssSelector("#loginForm > div:nth-child(5) > a")).size() > 0);
 		logAsCommissioner();
-		Assert.assertTrue(driver.findElements(By.cssSelector("#navigationbar > ul > li:nth-child(3) > a")).size() > 0);
+		Assert.assertTrue(driver.findElements(
+				By.cssSelector("#navigationbar > ul > li:nth-child(3) > a"))
+				.size() > 0);
 		logOff();
 	}
 
 	@Test
-	public void onlyCommissionerCanRegisterNewCoownerTest() throws InterruptedException {
+	public void onlyCommissionerCanRegisterNewCoownerTest()
+			throws InterruptedException {
 		logAsAdmin();
-		driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(3) > a")).click();
+		driver.findElement(
+				By.cssSelector("#navigationbar > ul > li:nth-child(3) > a"))
+				.click();
 		driver.findElement(
 				By.cssSelector("#ñhangeReg > div:nth-child(1) > div.panel-body.panel30 > div:nth-child(3) > label"))
 				.click();
 		driver.findElement(By.id("confirmRegistrationMethod")).click();
 		logOff();
-		Assert.assertFalse(driver.findElements(By.cssSelector("#loginForm > div:nth-child(5) > a")).size() > 0);
+		Assert.assertFalse(driver.findElements(
+				By.cssSelector("#loginForm > div:nth-child(5) > a")).size() > 0);
 		logAsCommissioner();
-		Assert.assertTrue(driver.findElements(By.cssSelector("#navigationbar > ul > li:nth-child(3) > a")).size() > 0);
+		Assert.assertTrue(driver.findElements(
+				By.cssSelector("#navigationbar > ul > li:nth-child(3) > a"))
+				.size() > 0);
 		logOff();
 	}
 
 	@Test
 	public void onlyPersonalRegistrationTest() {
 		logAsAdmin();
-		driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(3) > a")).click();
+		driver.findElement(
+				By.cssSelector("#navigationbar > ul > li:nth-child(3) > a"))
+				.click();
 		driver.findElement(
 				By.cssSelector("#ñhangeReg > div:nth-child(1) > div.panel-body.panel30 > div:nth-child(2) > label"))
 				.click();
 		driver.findElement(By.id("confirmRegistrationMethod")).click();
 		logOff();
-		Assert.assertTrue(driver.findElements(By.cssSelector("#loginForm > div:nth-child(5) > a")).size() > 0);
+		Assert.assertTrue(driver.findElements(
+				By.cssSelector("#loginForm > div:nth-child(5) > a")).size() > 0);
 		logAsCommissioner();
-		Assert.assertFalse(driver.findElements(By.cssSelector("#navigationbar > ul > li:nth-child(3) > a")).size() > 0);
+		Assert.assertFalse(driver.findElements(
+				By.cssSelector("#navigationbar > ul > li:nth-child(3) > a"))
+				.size() > 0);
 		logOff();
 	}
 
