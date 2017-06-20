c6c45ee63ba0925a36970d5e28a0bf5b18bcc70a
andyyarych <flakerino@gmail.com>
2017-06-20 16:07:04 +0300

    Added tools for l10n

--- src/test/java/com/softserve/edu/registrator/pages/RegisterNewUserAddressInfo.java
+++ src/test/java/com/softserve/edu/registrator/pages/RegisterNewUserAddressInfo.java
@@ -1,35 +1,133 @@
 package com.softserve.edu.registrator.pages;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.softserve.edu.registrator.pages.ATopComponent.ChangeLanguageFields;
+
 public class RegisterNewUserAddressInfo {
 
+// Enum with labels for localization
+    
+    public static enum AddressInfoL10n {
+        ADDRESSINFO_LABEL("Адресні дані", "Адресные данные", "Address infomation"),
+        CITY_LABEL("Місто", "Город", "City"),
+        REGION_LABEL("Область", "Область", "Region"),
+        DISTRICT_LABEL("Район", "Район", "District"),
+        STREET_LABEL("Вулиця", "Улица", "Street"),
+        BUILDING_LABEL("Будинок", "Дом", "Building"),
+        FLAT_LABEL("Квартира", "Квартира", "Flat"),
+        POSTCODE_LABEL("Поштовий індекс", "Почтовый индекс", "Postcode");
+        
+        private HashMap<ChangeLanguageFields, String> field;
+
+        private AddressInfoL10n(String... localization) {
+            this.field = new HashMap<ChangeLanguageFields, String>();
+            int i = 0;
+            for (ChangeLanguageFields language : ChangeLanguageFields.values()) {
+                this.field.put(language, localization[i]);
+                i++;
+            }
+        }
+
+        public String getLocalization(ChangeLanguageFields language) {
+            return this.field.get(language).trim();
+        }
+    }
+    
+    public static final String ADDRESSINFO_LABEL_CSSSELECTOR = ".address_header h4";
+    public static final String CITY_LABEL_CSSSELECTOR = ".address_header h4+label";
+    public static final String CITY_INPUT_ID = "city";
+    public static final String REGION_LABEL_CSSSELECTOR = "label[for='region']";
+    public static final String REGION_INPUT_ID = "region";
+    public static final String DISTRICT_LABEL_CSSSELECTOR = "label[for='district']";
+    public static final String DISTRICT_INPUT_ID = "district";
+    public static final String STREET_LABEL_CSSSELECTOR = "label[for='street']";
+    public static final String STREET_INPUT_ID = "street";
+    public static final String BUILDING_LABEL_CSSSELECTOR = "label[for='building']";
+    public static final String BUILDING_INPUT_ID = "building";
+    public static final String FLAT_LABEL_CSSSELECTOR = "label[for='flat']";
+    public static final String FLAT_INPUT_ID = "flat";
+    public static final String POSTCODE_LABEL_CSSSELECTOR = "label[for='postcode']";
+    public static final String POSTCODE_INPUT_ID = "postcode";
+    
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
     // Fields
     
+    private WebElement addressInfoLabel; 
+    private WebElement cityLabel;
     private WebElement city;
+    private WebElement regionLabel;
     private WebElement region;
+    private WebElement districtLabel;
     private WebElement district;
+    private WebElement streetLabel;
     private WebElement street;
+    private WebElement buildingLabel;
     private WebElement building;
+    private WebElement flatLabel;
     private WebElement flat;
+    private WebElement postcodeLabel;
     private WebElement postcode;
     
     // Public constructor
     
     public RegisterNewUserAddressInfo(WebDriver driver) {
-        this.city = driver.findElement(By.id("city"));
-        this.region = driver.findElement(By.id("region"));
-        this.district = driver.findElement(By.id("district"));
-        this.street = driver.findElement(By.id("street"));
-        this.building = driver.findElement(By.id("building"));
-        this.flat = driver.findElement(By.id("flat"));
-        this.postcode = driver.findElement(By.id("postcode"));
+        this.addressInfoLabel = driver.findElement(By.cssSelector(ADDRESSINFO_LABEL_CSSSELECTOR));
+        this.cityLabel = driver.findElement(By.cssSelector(CITY_LABEL_CSSSELECTOR));
+        this.city = driver.findElement(By.id(CITY_INPUT_ID));
+        this.regionLabel = driver.findElement(By.cssSelector(REGION_LABEL_CSSSELECTOR));
+        this.region = driver.findElement(By.id(REGION_INPUT_ID));
+        this.districtLabel = driver.findElement(By.cssSelector(DISTRICT_LABEL_CSSSELECTOR));
+        this.district = driver.findElement(By.id(DISTRICT_INPUT_ID));
+        this.streetLabel = driver.findElement(By.cssSelector(STREET_LABEL_CSSSELECTOR));
+        this.street = driver.findElement(By.id(STREET_INPUT_ID));
+        this.buildingLabel = driver.findElement(By.cssSelector(BUILDING_LABEL_CSSSELECTOR));
+        this.building = driver.findElement(By.id(BUILDING_INPUT_ID));
+        this.flatLabel = driver.findElement(By.cssSelector(FLAT_LABEL_CSSSELECTOR));
+        this.flat = driver.findElement(By.id(FLAT_INPUT_ID));
+        this.postcodeLabel = driver.findElement(By.cssSelector(POSTCODE_LABEL_CSSSELECTOR));
+        this.postcode = driver.findElement(By.id(POSTCODE_INPUT_ID));
     }
 
+    // get Address info labels
+
+    public WebElement getAddressInfoLabel() {
+        return addressInfoLabel;
+    }
+    
+    public WebElement getCityLabel() {
+        return cityLabel;
+    }
+    
+    public WebElement getRegionLabel() {
+        return regionLabel;
+    }
+    
+    public WebElement getDistrictLabel() {
+        return districtLabel;
+    }
+    
+    public WebElement getStreetLabel() {
+        return streetLabel;
+    }
+    
+    public WebElement getBuildingLabel() {
+        return buildingLabel;
+    }
+    
+    public WebElement getFlatLabel() {
+        return flatLabel;
+    }
+    
+    public WebElement getPostcodeLabel() {
+        return postcodeLabel;
+    }
+    
     // get Address info
     
     public WebElement getCity() {
@@ -105,4 +203,38 @@ public class RegisterNewUserAddressInfo {
         getPostcode().sendKeys(postcode);
     }
     
+    // Functional
+    
+    public String getAddressInfoLabelText() {
+        return getAddressInfoLabel().getText().trim();
+    }
+    
+    public String getCityLabelText() {
+        return getCityLabel().getText().trim();
+    }
+    
+    public String getRegionLabelText() {
+        return getRegionLabel().getText().trim();
+    }
+    
+    public String getDistrictLabelText() {
+        return getDistrictLabel().getText().trim();
+    }
+    
+    public String getStreetLabelText() {
+        return getStreetLabel().getText().trim();
+    }
+    
+    public String getBuildingLabelText() {
+        return getBuildingLabel().getText().trim();
+    }
+    
+    public String getFlatLabelText() {
+        return getFlatLabel().getText().trim();
+    }
+    
+    public String getPostcodeLabelText() {
+        return getPostcodeLabel().getText().trim();
+    }
+        
 }
--- src/test/java/com/softserve/edu/registrator/pages/RegisterUserPage.java
+++ src/test/java/com/softserve/edu/registrator/pages/RegisterUserPage.java
@@ -1,12 +1,46 @@
 package com.softserve.edu.registrator.pages;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.softserve.edu.registrator.data.users.IUser;
 import com.softserve.edu.registrator.data.users.User;
+import com.softserve.edu.registrator.pages.ATopComponent.ChangeLanguageFields;
 
 public class RegisterUserPage extends AdminHomePage {
+    
+    public static enum RegisterUserPageL10n {
+        SUBMIT_BUTTON("Надіслати", "Отправить", "Send"),
+        CLEARFORM_BUTTON("Очистити форму", "Очистить форму", "Clear form"),
+        CANCEL_BUTTON("Відмінити", "Отменить", "Cancel");
+        // REQUIREDFIELDS_LABEL("Поля, позначені зірочкою (*), є обов'язковими для заповнення",
+        //      "Поля, отмеченные звездочкой (*), обязательны для заполнения", 
+        //      "Required fields are marked with an (*)");
+
+        private HashMap<ChangeLanguageFields, String> field;
+
+        private RegisterUserPageL10n(String... localization) {
+            this.field = new HashMap<ChangeLanguageFields, String>();
+            int i = 0;
+            for (ChangeLanguageFields language : ChangeLanguageFields.values()) {
+                this.field.put(language, localization[i]);
+                i++;
+            }
+        }
+
+        public String getLocalization(ChangeLanguageFields language) {
+            return this.field.get(language).trim();
+        }
+    }
+       
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    public static final String SUBMIT_ID = "submit";
+    public static final String CLEARFORM_CSSSELECTOR = ".btn.btn-warning.reset";
+    public static final String CANCEL_CSSSELECTOR = "#registrationForm .btn.btn-primary";
     
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
@@ -27,9 +61,9 @@ public class RegisterUserPage extends AdminHomePage {
     
     public RegisterUserPage(WebDriver driver) {
         super(driver);
-        this.submit = driver.findElement(By.id("submit"));
-        this.clearForm = driver.findElement(By.cssSelector(".btn.btn-warning.reset"));
-        this.cancel = driver.findElement(By.cssSelector("#registrationForm .btn.btn-primary"));
+        this.submit = driver.findElement(By.id(SUBMIT_ID));
+        this.clearForm = driver.findElement(By.cssSelector(CLEARFORM_CSSSELECTOR));
+        this.cancel = driver.findElement(By.cssSelector(CANCEL_CSSSELECTOR));
         
         this.mainInfo = new RegisterNewUserMainInfo(driver);
         this.addressInfo = new RegisterNewUserAddressInfo(driver);
@@ -68,12 +102,11 @@ public class RegisterUserPage extends AdminHomePage {
     public RegisterNewUserOtherInfo getOtherInfo() {
         return otherInfo;
     }
-    
+       
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
     // Functional
         
-
     public String getSubmitText() {
         return getSubmit().getText();
     }
@@ -112,16 +145,32 @@ public class RegisterUserPage extends AdminHomePage {
         return new RegisterUserPage(driver);
     }
     
-    //TODO finish this method
-    public void registerNewUser(User user) {
-        mainInfo.getFirstName().sendKeys(user.getPerson().getFirstname());
-        mainInfo.getLastName().sendKeys(user.getPerson().getLastname());
-        mainInfo.getMiddleName().sendKeys(user.getPerson().getMiddlename());
-        mainInfo.getEmail().sendKeys(user.getPerson().getEmail());
-        mainInfo.getLogin().sendKeys(user.getAccount().getLogin());
-        mainInfo.getPassword().sendKeys(user.getAccount().getPassword());
-        mainInfo.getConfirmPassword().sendKeys(user.getAccount().getPassword());
-        //TODO add getCommunity and unnecessary fields
+    public void registerNewUser(IUser newUser) {
+        // Adding main info about user
+        getMainInfo().getFirstName().sendKeys(newUser.getPerson().getFirstname());
+        getMainInfo().getLastName().sendKeys(newUser.getPerson().getLastname());
+        getMainInfo().getMiddleName().sendKeys(newUser.getPerson().getMiddlename());
+        getMainInfo().getEmail().sendKeys(newUser.getPerson().getEmail());
+        getMainInfo().getLogin().sendKeys(newUser.getAccount().getLogin());
+        getMainInfo().getPassword().sendKeys(newUser.getAccount().getPassword());
+        getMainInfo().getConfirmPassword().sendKeys(newUser.getAccount().getPassword());
+        // Adding address info about user
+        getAddressInfo().getCity().sendKeys(newUser.getAddress().getCity());
+        getAddressInfo().getRegion().sendKeys(newUser.getAddress().getRegion());
+        getAddressInfo().getStreet().sendKeys(newUser.getAddress().getStreet());
+        getAddressInfo().getBuilding().sendKeys(newUser.getAddress().getBuilding());
+        getAddressInfo().getFlat().sendKeys(newUser.getAddress().getFlat());
+        getAddressInfo().getPostcode().sendKeys(newUser.getAddress().getPostcode());
+        // Adding passport info about user
+        getPassportInfo().getSeria().sendKeys(newUser.getPassport().getSeria());
+        getPassportInfo().getPassportNumber().sendKeys(newUser.getPassport().getNumber());
+        getPassportInfo().getPublished().sendKeys(newUser.getPassport().getPublished());
+        // Adding other info about user
+        getOtherInfo().getPhoneNumber().sendKeys(newUser.getPerson().getPhonenumber());
+        getOtherInfo().setCommunity(newUser.getAccount().getCommunity());
+        getOtherInfo().getDate().sendKeys(newUser.getAccount().getData());
+        // Registering new user
+        clickSubmit();
     }
         
     public NonConfirmedCoownersPage successfulRegistration(User user) {
@@ -130,7 +179,7 @@ public class RegisterUserPage extends AdminHomePage {
     }
     
     public RegisterUserPage unsuccessfulReristration(User user) {
-        // registerNewUser(user); // Is it needed???
+        registerNewUser(user); // Is it needed???
         return new RegisterUserPage(driver);
     }
     
