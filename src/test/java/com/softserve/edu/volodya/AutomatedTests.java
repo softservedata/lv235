496a6aff2e693fe16a84fcbfceded5ee06206bd0
Vghost <no@no.com>
2017-06-22 17:48:01 +0300

    Merged with my branch

--- src/test/java/com/softserve/edu/volodya/AutomatedTests.java
+++ src/test/java/com/softserve/edu/volodya/AutomatedTests.java
@@ -10,20 +10,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.softserve.edu.registrator.pages.LoginPage;
+import com.softserve.edu.registrator.pages.RegistratorHomePage;
 
 public class AutomatedTests {
 
 	/**
 	 * Delay for Thread.Sleep().
 	 */
-	private final int DELAY_FOR_DEMO = 500;
+	private final int DELAY_FOR_DEMO = 300;
 
 	/**
 	 * This method initialize driver with settings.
-	 * 
 	 * @return driver.
-	 * @throws Exception
-	 *             - use for Thread.Sleep().
+	 * @throws Exception - use for Thread.Sleep().
 	 */
 	private WebDriver init() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
@@ -38,16 +37,12 @@ public class AutomatedTests {
 	}
 
 	/**
-	 * Thread.sleep() Use only for DEMO! This method login user to account.
-	 * 
-	 * @param user
-	 *            - user name.
-	 * @param password
-	 *            - user password.
-	 * @param driver
-	 *            - driver.
-	 * @throws Exception
-	 *             - use for Thread.Sleep().
+	 * Thread.sleep() Use only for DEMO!
+	 * This method login user to account.
+	 * @param user - user name.
+	 * @param password - user password.
+	 * @param driver - driver.
+	 * @throws Exception - use for Thread.Sleep().
 	 */
 	private void loginAsRegistrator(WebDriver driver) throws Exception {
 		LoginPage loginPage = new LoginPage(driver);
@@ -63,11 +58,8 @@ public class AutomatedTests {
 
 	/**
 	 * This method logout current user.
-	 * 
-	 * @param driver
-	 *            - driver.
-	 * @throws Exception
-	 *             - use for Thread.Sleep().
+	 * @param driver - driver.
+	 * @throws Exception - use for Thread.Sleep().
 	 */
 	private void logout(WebDriver driver) throws Exception {
 		RegistratorHomePage rp = new RegistratorHomePage(driver);
@@ -81,86 +73,67 @@ public class AutomatedTests {
 	}
 
 	/**
-	 * This test CREATE a new subclass. Thread.sleep() Use only for DEMO!
-	 * 
-	 * @throws Exception
-	 *             - use for Thread.Sleep().
+	 * This test CREATE a new subclass.
+	 * Thread.sleep() Use only for DEMO!
+	 * @throws Exception - use for Thread.Sleep().
 	 */
 	@Test
 	public void testCreateSubclass() throws Exception {
 		WebDriver driver = init();
-		SubclassPage subclassPage = new SubclassPage(driver);
 		loginAsRegistrator(driver);
-		driver.findElement(By.xpath("//*[@id='navigationbar']/ul/li[3]/a"))
-				.click(); // Subclass of objects
+		RegistratorHomePage rp = new RegistratorHomePage(driver);
+		rp.clickSubclassObjects();
 		Thread.sleep(DELAY_FOR_DEMO);
+		SubclassPage subclassPage = new SubclassPage(driver);
 		subclassPage.getAddSubclassButton().click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		subclassPage.getSubclassName().click();
-		subclassPage.getSubclassName().sendKeys("TestName");
+		subclassPage.FillSubclassName("TestName");
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(By.id("clickmeshow")).click();
+		subclassPage.clickAddParametersButton();
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(By.name("parameters[0].description")).click();
-		driver.findElement(By.name("parameters[0].description")).sendKeys(
-				"Test_Description");
+		subclassPage.FillDescriptionParameter("Test_Description");
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(By.name("parameters[0].unitName")).click();
-		driver.findElement(By.name("parameters[0].unitName"))
-				.sendKeys("points");
+		subclassPage.FillUnitParameter("points");
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(By.cssSelector("*[value='discreteParameters']"))
-				.click();
-		driver.findElement(By.id("btnAdd")).click();
+		subclassPage.clickDiscreteParameter();
+		subclassPage.clickAddButton();
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(By.name("parameters[1].description")).click();
-		driver.findElement(By.name("parameters[1].description")).sendKeys(
-				"Test_Description2");
+		subclassPage.FillDescriptionParameter2("Test_Description2");
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(By.name("parameters[1].unitName")).click();
-		driver.findElement(By.name("parameters[1].unitName")).sendKeys("count");
+		subclassPage.FillUnitParameter2("count");
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(
-				By.cssSelector("#input2 #myparam2 [value='linearParameters']"))
-				.click();
-		// .findElement(By.cssSelector("*")).click();
+		subclassPage.clickLinearParameter();
+		//.findElement(By.cssSelector("*")).click();
 		driver.findElement(By.id("valid")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		Assert.assertNotNull(driver.findElement(
-				By.xpath(".//*[@id='datatable']//td[contains(.,'TestName')]"))
-				.getText());
+		Assert.assertNotNull(subclassPage.FindSubclassName("TestName"));
 		Thread.sleep(DELAY_FOR_DEMO);
 		logout(driver);
 	}
 
 	/**
-	 * Test to add resource and search it Thread.sleep() Use only for DEMO!
-	 * 
-	 * @throws Exception
-	 *             - use for Thread.Sleep().
+	 * Test to add resource and search it
+	 * Thread.sleep() Use only for DEMO!
+	 * @throws Exception - use for Thread.Sleep().
 	 */
-	// @Test
+	//@Test
 	public void testAddResource() throws Exception {
 		WebDriver driver = init();
 		loginAsRegistrator(driver);
 		Thread.sleep(DELAY_FOR_DEMO);
-		// driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(5) > a")).click();
+	//	driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(5) > a")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("owner_search")).click();
 		driver.findElement(By.id("owner_search")).sendKeys("NazarUser");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("w-input-search")).click();
-		driver.findElement(By.id("w-input-search")).sendKeys("Тестовий"); // Only
-																			// Ukrainian
-																			// allowed
+		driver.findElement(By.id("w-input-search")).sendKeys("Тестовий"); // Only Ukrainian allowed
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("resourcesTypeSelect")).click();
 		driver.findElement(By.cssSelector("*[value='TestName']")).click();
 		driver.findElement(By.id("delivery")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(
-				By.xpath("//*[@id='coordinates']/li/a[@data-target='#points']"))
-				.click();
+		driver.findElement(By.xpath("//*[@id='coordinates']/li/a[@data-target='#points']")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("myparam1")).click();
 		driver.findElement(By.id("myparam1")).clear();
@@ -190,65 +163,53 @@ public class AutomatedTests {
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput2 #myparam1")).click();
 		driver.findElement(By.cssSelector("#areaInput2 #myparam1")).clear();
-		driver.findElement(By.cssSelector("#areaInput2 #myparam1")).sendKeys(
-				"2");
+		driver.findElement(By.cssSelector("#areaInput2 #myparam1")).sendKeys("2");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput2 #myparam2")).click();
 		driver.findElement(By.cssSelector("#areaInput2 #myparam2")).clear();
-		driver.findElement(By.cssSelector("#areaInput2 #myparam2")).sendKeys(
-				"2");
+		driver.findElement(By.cssSelector("#areaInput2 #myparam2")).sendKeys("2");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput2 #myparam3")).click();
 		driver.findElement(By.cssSelector("#areaInput2 #myparam3")).clear();
-		driver.findElement(By.cssSelector("#areaInput2 #myparam3")).sendKeys(
-				"2.1");
+		driver.findElement(By.cssSelector("#areaInput2 #myparam3")).sendKeys("2.1");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput2 #myparam4")).click();
 		driver.findElement(By.cssSelector("#areaInput2 #myparam4")).clear();
-		driver.findElement(By.cssSelector("#areaInput2 #myparam4")).sendKeys(
-				"2");
+		driver.findElement(By.cssSelector("#areaInput2 #myparam4")).sendKeys("2");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput2 #myparam5")).click();
 		driver.findElement(By.cssSelector("#areaInput2 #myparam5")).clear();
-		driver.findElement(By.cssSelector("#areaInput2 #myparam5")).sendKeys(
-				"2");
+		driver.findElement(By.cssSelector("#areaInput2 #myparam5")).sendKeys("2");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput2 #myparam6")).click();
 		driver.findElement(By.cssSelector("#areaInput2 #myparam6")).clear();
-		driver.findElement(By.cssSelector("#areaInput2 #myparam6")).sendKeys(
-				"2.1");
+		driver.findElement(By.cssSelector("#areaInput2 #myparam6")).sendKeys("2.1");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("btnAddAreaPoint")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput3 #myparam1")).click();
 		driver.findElement(By.cssSelector("#areaInput3 #myparam1")).clear();
-		driver.findElement(By.cssSelector("#areaInput3 #myparam1")).sendKeys(
-				"3");
+		driver.findElement(By.cssSelector("#areaInput3 #myparam1")).sendKeys("3");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput3 #myparam2")).click();
 		driver.findElement(By.cssSelector("#areaInput3 #myparam2")).clear();
-		driver.findElement(By.cssSelector("#areaInput3 #myparam2")).sendKeys(
-				"3");
+		driver.findElement(By.cssSelector("#areaInput3 #myparam2")).sendKeys("3");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput3 #myparam3")).click();
 		driver.findElement(By.cssSelector("#areaInput3 #myparam3")).clear();
-		driver.findElement(By.cssSelector("#areaInput3 #myparam3")).sendKeys(
-				"3.1");
+		driver.findElement(By.cssSelector("#areaInput3 #myparam3")).sendKeys("3.1");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput3 #myparam4")).click();
 		driver.findElement(By.cssSelector("#areaInput3 #myparam4")).clear();
-		driver.findElement(By.cssSelector("#areaInput3 #myparam4")).sendKeys(
-				"3");
+		driver.findElement(By.cssSelector("#areaInput3 #myparam4")).sendKeys("3");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput3 #myparam5")).click();
 		driver.findElement(By.cssSelector("#areaInput3 #myparam5")).clear();
-		driver.findElement(By.cssSelector("#areaInput3 #myparam5")).sendKeys(
-				"3");
+		driver.findElement(By.cssSelector("#areaInput3 #myparam5")).sendKeys("3");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.cssSelector("#areaInput3 #myparam6")).click();
 		driver.findElement(By.cssSelector("#areaInput3 #myparam6")).clear();
-		driver.findElement(By.cssSelector("#areaInput3 #myparam6")).sendKeys(
-				"3.1");
+		driver.findElement(By.cssSelector("#areaInput3 #myparam6")).sendKeys("3.1");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.xpath("//*[@id='calculatedParamsTab']")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
@@ -262,71 +223,48 @@ public class AutomatedTests {
 		driver.findElement(By.id("linearBegin")).sendKeys("2000");
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("linearEnd")).click();
-		driver.findElement(By.id("linearEnd"))
-				.sendKeys("Second Great comment!");
+		driver.findElement(By.id("linearEnd")).sendKeys("Second Great comment!");
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(
-				By.xpath("//*[@id='coordinates']/li/a[@data-target='#map']"))
-				.click();
+		driver.findElement(By.xpath("//*[@id='coordinates']/li/a[@data-target='#map']")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("allUkraine")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("submitForm")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		// driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(2) > a")).click();
+		//driver.findElement(By.cssSelector("#navigationbar > ul > li:nth-child(2) > a")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("searchByParameterButton")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
 		driver.findElement(By.id("resourcesTypeSelect")).click();
-		driver.findElement(
-				By.xpath("//*[@id='resourcesTypeSelect']/option[contains(.,'TestName')]"))
-				.click();
-		driver.findElement(
-				By.cssSelector("#discreteParameters > div > span.col-md-6 > input"))
-				.click();
+		driver.findElement(By.xpath("//*[@id='resourcesTypeSelect']/option[contains(.,'TestName')]")).click();
+		driver.findElement(By.cssSelector("#discreteParameters > div > span.col-md-6 > input")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(
-				By.cssSelector("#discreteParameters > div > span.col-md-6 > input"))
-				.sendKeys("1000");
-		driver.findElement(
-				By.cssSelector("#linearParameters > div > span > input"))
-				.click();
-		driver.findElement(
-				By.cssSelector("#linearParameters > div > span > input"))
-				.sendKeys("2000");
+		driver.findElement(By.cssSelector("#discreteParameters > div > span.col-md-6 > input")).sendKeys("1000");
+		driver.findElement(By.cssSelector("#linearParameters > div > span > input")).click();
+		driver.findElement(By.cssSelector("#linearParameters > div > span > input")).sendKeys("2000");
 		Thread.sleep(DELAY_FOR_DEMO);
 		Assert.assertNotNull(By.linkText("Тестовий"));
 		logout(driver);
 	}
 
 	/**
-	 * This test DELETE a new subclass. Thread.sleep() Use only for DEMO!
-	 * 
-	 * @throws Exception
-	 *             - use for Thread.Sleep().
+	 * This test DELETE a new subclass.
+	 * Thread.sleep() Use only for DEMO!
+	 * @throws Exception - use for Thread.Sleep().
 	 */
-	// @Test
+	//@Test
 	public void testDeleteSubclass() throws Exception {
 		WebDriver driver = init();
 		loginAsRegistrator(driver);
 		RegistratorHomePage rp = new RegistratorHomePage(driver);
 		rp.clickSubclassObjects(); // Subclass of objects
 		Thread.sleep(DELAY_FOR_DEMO);
-		Assert.assertNotNull(driver.findElement(
-				By.xpath(".//*[@id='datatable']//td[contains(.,'TestName')]"))
-				.getText());
-		driver.findElement(
-				By.xpath(".//*[@id='datatable']//td[contains(.,'TestName')]/../td[6]/div/a"))
-				.click();
+		Assert.assertNotNull(driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'TestName')]")).getText());
+		driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'TestName')]/../td[6]/div/a")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		driver.findElement(
-				By.cssSelector(".bootbox.modal.fade.bootbox-confirm.in"))
-				.findElement(By.cssSelector(".btn.btn-primary")).click();
+		driver.findElement(By.cssSelector(".bootbox.modal.fade.bootbox-confirm.in")).findElement(By.cssSelector(".btn.btn-primary")).click();
 		Thread.sleep(DELAY_FOR_DEMO);
-		Assert.assertTrue(driver
-				.findElements(
-						By.xpath("*[@id='datatable']//td[contains(text(),'TestName')]"))
-				.size() == 0);
+		Assert.assertTrue(driver.findElements(By.xpath("*[@id='datatable']//td[contains(text(),'TestName')]")).size() == 0);
 		logout(driver);
 	}
 }
--- src/test/java/com/softserve/edu/volodya/SubclassPage.java
+++ src/test/java/com/softserve/edu/volodya/SubclassPage.java
@@ -8,18 +8,14 @@ import com.softserve.edu.registrator.pages.ACommonComponent;
 
 public class SubclassPage extends ACommonComponent {
 
-	private static int ParameterCounter = -1;
-
 	private static final String SUBCLASS_NAME_NAME = "typeName";
 	private static final String ADD_PARAMETERS_ID = "clickmeshow";
-	private static final String DESCRIPTION_PARAMETER_NAME = "parameters["
-			+ ParameterCounter + "].description";
-	private static final String UNIT_PARAMETER_NAME = "parameters["
-			+ ParameterCounter + "].unitName";
-	private static final String DISCRETE_PARAMETER_CSS = "#input"
-			+ ParameterCounter + 1 + " #myparam2 [value='discreteParameters']";
-	private static final String LINEAR_PARAMETER_CSS = "#input"
-			+ ParameterCounter + 1 + " #myparam2 [value='linearParameters']";
+	private static final String DESCRIPTION_PARAMETER_NAME = "parameters[0].description";
+	private static final String UNIT_PARAMETER_NAME = "parameters[0].unitName";
+	private static final String DESCRIPTION_PARAMETER_NAME2 = "parameters[1].description";
+	private static final String UNIT_PARAMETER_NAME2 = "parameters[1].unitName";
+	private static final String DISCRETE_PARAMETER_CSS = "#input1 #myparam2 [value='discreteParameters']";
+	private static final String LINEAR_PARAMETER_CSS = "#input2 #myparam2 [value='linearParameters']";
 	private static final String ADD_BUTTON_ID = "btnAdd";
 
 	//
@@ -28,7 +24,7 @@ public class SubclassPage extends ACommonComponent {
 	public SubclassPage(WebDriver driver) {
 		super(driver);
 	}
-
+	
 	//
 	// Get
 	//
@@ -39,82 +35,108 @@ public class SubclassPage extends ACommonComponent {
 	public WebElement getSubclassName() {
 		return driver.findElement(By.name(SUBCLASS_NAME_NAME));
 	}
-
+	
 	public WebElement getAddParameters() {
 		return driver.findElement(By.id(ADD_PARAMETERS_ID));
 	}
-
+	
 	public WebElement getDescriptionParameter() {
 		return driver.findElement(By.name(DESCRIPTION_PARAMETER_NAME));
 	}
-
+	
 	public WebElement getUnitParameter() {
 		return driver.findElement(By.name(UNIT_PARAMETER_NAME));
 	}
-
+	
 	public WebElement getAddButton() {
 		return driver.findElement(By.id(ADD_BUTTON_ID));
 	}
-
+	
 	public WebElement getDiscreteParameter() {
 		return driver.findElement(By.cssSelector(DISCRETE_PARAMETER_CSS));
 	}
-
+	
 	public WebElement getLinearParameter() {
 		return driver.findElement(By.cssSelector(LINEAR_PARAMETER_CSS));
 	}
-
+	
 	//
 	// Click
 	//
 	public void clickSubclassName() {
 		getSubclassName().click();
 	}
-
+	
 	public void clickAddParametersButton() {
 		getAddParameters().click();
-		ParameterCounter++;
 	}
-
+	
 	public void clickDescriptionParameter() {
 		getDescriptionParameter().click();
 	}
-
+	
 	public void clickUnitParameter() {
 		getUnitParameter().click();
 	}
-
+	
 	public void clickDiscreteParameter() {
 		getDiscreteParameter().click();
 	}
-
+	
 	public void clickLinearParameter() {
 		getLinearParameter().click();
 	}
-
+ 	
+	public void clickAddButton() {
+		getAddButton().click();
+	}
 	//
 	// InputData
 	//
 	public void FillSubclassName(String subclassName) {
+		clickSubclassName();
 		getSubclassName().sendKeys(subclassName);
 	}
-
+	
 	public void FillDescriptionParameter(String descriptionName) {
+		clickDescriptionParameter();
 		getDescriptionParameter().sendKeys(descriptionName);
 	}
-
 	public void FillUnitParameter(String unitName) {
+		clickUnitParameter();
 		getUnitParameter().sendKeys(unitName);
 	}
-
-	//
+	
+	// 
 	// Functions
 	//
-
+	
 	public String FindSubclassName(String subclassName) {
-		return driver.findElement(
-				By.xpath(".//*[@id='datatable']//td[contains(.,'"
-						+ subclassName + "')]")).getText();
+		return driver.findElement(By.xpath(".//*[@id='datatable']//td[contains(.,'"+ subclassName +"')]")).getText();
 	}
-
+	
+	
+	//
+	//
+	//
+	//
+	//
+	//
+	public WebElement getDescriptionParameter2() {
+		return driver.findElement(By.name(DESCRIPTION_PARAMETER_NAME2));
+	}
+	
+	public WebElement getUnitParameter2() {
+		return driver.findElement(By.name(UNIT_PARAMETER_NAME2));
+	}
+	
+	public void FillDescriptionParameter2(String descriptionName) {
+		getDescriptionParameter2().click();
+		getDescriptionParameter2().sendKeys(descriptionName);
+	}
+	public void FillUnitParameter2(String unitName) {
+		getUnitParameter2().click();
+		getUnitParameter2().sendKeys(unitName);
+	}
+	
 }
