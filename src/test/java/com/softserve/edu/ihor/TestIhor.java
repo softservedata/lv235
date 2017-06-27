d983f12c6918278352d22e4eff6464a81eb006b6
Ihor <tistyk.27@gmail.com>
2017-06-27 14:57:19 +0400

    test

---
+++ src/test/java/com/softserve/edu/ihor/TestIhor.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.ihor;
+
+import com.softserve.edu.registrator.tests.TestRunner;
+
+/**
+ * Created by User on 6/27/2017.
+ */
+public class TestIhor extends TestRunner {
+
+}
